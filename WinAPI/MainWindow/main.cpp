10db61f7185255b62bd50f74fad3d2555258fa26
User <User@DESKTOP-QHG18FL>
2025-10-04 11:50:39 +0300

    Icons & Cursor DONE

---
+++ WinAPI/MainWindow/main.cpp
@@ -0,0 +1,90 @@
+#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR g_sz_WND_CLASS_NAME[] = "My Windows Class";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1) Регистрация класса окна:
+	WNDCLASSEX wClass;	//Структура, описывающая класс окна
+	ZeroMemory(&wClass, sizeof(wClass));
+
+	wClass.style = 0;
+	wClass.cbSize = sizeof(wClass);
+	wClass.cbWndExtra = 0;
+	wClass.cbClsExtra = 0;
+
+	//wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	//wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIconSm = (HICON)LoadImage(hInstance, "Bitcoin.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wClass.hIcon = (HICON)LoadImage(hInstance, "2Palms.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
+	wClass.hCursor = (HCURSOR)LoadImage(hInstance, "Working In Background.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	wClass.hInstance = hInstance;
+	wClass.lpszClassName = g_sz_WND_CLASS_NAME;
+	wClass.lpszMenuName = NULL;
+	wClass.lpfnWndProc = WndProc;
+
+	if (!RegisterClassEx(&wClass))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	//2) Создание окна:
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,	//exStyles
+		g_sz_WND_CLASS_NAME,	//Class name
+		g_sz_WND_CLASS_NAME,	//Winodw title
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,CW_USEDEFAULT,	//Window position
+		CW_USEDEFAULT,CW_USEDEFAULT,	//Window size
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	//3) Запуск цикла сообщений:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		//DestroyWindow(hwnd);
+		SendMessage(hwnd, WM_DESTROY, 0, 0);
+		break;
+	default:	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
+}
